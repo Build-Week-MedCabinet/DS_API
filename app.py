ea152b917be494377b7c98c5c600f1688d2fd0b3
Vincent Brandon <vincenta..brandon@gmail.com>
2019-10-23 02:40:35 +0000

    updated requirements, trimmed app

--- app.py
+++ app.py
@@ -1,31 +1,18 @@
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
-import pickle as pk
 import json
-from random import randint
 
 # creates and configures flask app
 app = Flask(__name__)
-DB = SQLAlchemy()
+# DB = SQLAlchemy() # Not Implemented
 
-def randJSON():
-    num1 = [randint(1,100) for _ in range(10)]
-    num2 = [randint(1,100) for _ in range(10)]
-    num3 = [randint(1,100) for _ in range(10)]
-    x = {
-        'set 1': num1,
-        'set 2': num2,
-        'set 3': num3
-    }
-    y = json.dumps(x)
-    return y 
-
-
-@app.route('/')
+@app.route('/recommend/')
 def root():
     #dump = pk.dumps('knn_05.pkl')
     #load = pk.loads('dump')
     values = randJSON()
     return values
-   
-#print(values)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
