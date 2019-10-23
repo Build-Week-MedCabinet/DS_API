d1c9ef76d6dda53a3b32b6f30d17b1a4696ff7e9
gabeflomo <gabeflomo821@gmail.com>
2019-10-22 21:00:20 -0500

    requirements added

--- app.py
+++ app.py
@@ -1,8 +1,31 @@
-Click==7.0
-Flask==1.1.1
-Flask-SQLAlchemy==2.4.1
-itsdangerous==1.1.0
-Jinja2==2.10.3
-MarkupSafe==1.1.1
-SQLAlchemy==1.3.10
-Werkzeug==0.16.0
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+import pickle as pk
+import json
+from random import randint
+
+# creates and configures flask app
+app = Flask(__name__)
+DB = SQLAlchemy()
+
+def randJSON():
+    num1 = [randint(1,100) for _ in range(10)]
+    num2 = [randint(1,100) for _ in range(10)]
+    num3 = [randint(1,100) for _ in range(10)]
+    x = {
+        'set 1': num1,
+        'set 2': num2,
+        'set 3': num3
+    }
+    y = json.dumps(x)
+    return y 
+
+
+@app.route('/')
+def root():
+    #dump = pk.dumps('knn_05.pkl')
+    #load = pk.loads('dump')
+    values = randJSON()
+    return values
+   
+#print(values)
