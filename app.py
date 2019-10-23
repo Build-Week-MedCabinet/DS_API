c7830d4464bc52bdffc4bf6e30abf99c3b9fcc75
Vincent Brandon <vincenta..brandon@gmail.com>
2019-10-23 03:55:45 +0000

    added custom error and got qty responses working

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from nlp_module import nlp_model
+
+from errors import InvalidUsage
 
 ###########
 ###Setup###
@@ -24,11 +26,28 @@ def root():
 
 @app.route('/api/recommend/', methods=['GET'])
 def recommend():
-    prediction = predictor.predict('Glorious orange-red sativa')
+    # Set Defaults
+    num_responses = 5
+
+    if request.method == 'GET':
+        if not 'search' in request.args:
+            raise InvalidUsage(message="Search query not provided")
+        if 'qty' in request.args:
+            # print('number_responses', request.args['qty'])  # Debug
+            num_responses = int(request.args['qty'])
+
+    prediction = predictor.predict('Glorious orange-red sativa', size=num_responses)
     return {  # Can manually call jsonify().  Flask naturally conversts dicts to json objects.
         "predictions": prediction.tolist()
     }
 
+
+# Register error handler
+@app.errorhandler(InvalidUsage)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
 
 
 
