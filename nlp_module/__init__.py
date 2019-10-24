f803ab058fd1b3d139f0381f077efe5086b9bd57
Vincent Brandon <vincenta..brandon@gmail.com>
2019-10-24 03:50:37 +0000

    updated model

--- nlp_module/__init__.py
+++ nlp_module/__init__.py
@@ -5,9 +5,9 @@ import spacy
 ##SET PARAMETERS##
 ##################
 params = {
-    'model': 'kdtree_model_1.0.pkl',
+    'model': 'kdtree_model_1.1.pkl',
     # 'vectorizer': , # Implemented as class below
-    'clean_data': 'cannabis_clean_export_03.csv',
+    # 'clean_data': 'cannabis_clean_export_03.csv', # Not implemented
 }
 
 # Load spacy model
