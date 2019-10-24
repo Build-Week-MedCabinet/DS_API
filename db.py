8697c0dd13dfbbc86d1cedb9bc34c52478abd5c0
gabeflomo <gabeflomo821@gmail.com>
2019-10-24 16:18:03 -0500

    function to locate index

--- db.py
+++ db.py
@@ -28,3 +28,28 @@ def close_db(e=None):
 
 def init_app(app):
     app.teardown_appcontext(close_db)
+
+def queryDB(lst):
+    import os
+    
+    os.chdir("D:\\Documents\\Build week projects\\DS_api")
+    conn = sqlite3.connect('med_cabinet.sqlite3')
+    c = conn.cursor()
+    
+    desc = []
+    for i in lst:
+
+        rows = c.execute("""SELECT * FROM Strain_info as S
+                                JOIN Cannabinoids as C ON C.id=S.id
+                                JOIN Terpenes as T ON T.id=S.id
+                                WHERE S.id=""" + str(i)).fetchall()
+        
+        desc.append(rows)
+    
+    return desc
+    
+   
+# test list to check if the function works   
+lst = [45,32,23]
+rows = queryDB(lst)
+print(rows)
